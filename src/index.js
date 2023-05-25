dd7e8147700ea4969c796f980d9730dafaec59c9
YS <119687204+ysoejima@users.noreply.github.com>
2023-05-25 11:22:04 +0000

    Update 7 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import "./styles.css";
+/**
+ * const let
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var var1 = "var変数"
+console.log(val1);
+
